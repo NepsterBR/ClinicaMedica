cf53beaf3728c51d81079bbf9fd98b4ae5a3e73d
ajurberg <ajurberg@gmail.com>
2021-07-07 11:24:22 -0300

    Updated classes

---
+++ src/main/java/factory/ExameViewAnnotationLiteral.java
@@ -0,0 +1,20 @@
+package factory;
+
+import dominio.ExameEnum;
+import jakarta.enterprise.util.AnnotationLiteral;
+import view.TipoExame;
+
+public class ExameViewAnnotationLiteral extends AnnotationLiteral<TipoExame> implements TipoExame {
+
+    private final ExameEnum exameEnum;
+
+    public ExameViewAnnotationLiteral(ExameEnum exameEnum) {
+        this.exameEnum = exameEnum;
+    }
+
+    @Override
+    public ExameEnum value() {
+        return this.exameEnum;
+    }
+
+}
